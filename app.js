d0e75e8cb33db019f89fe1494a4ab9fe7b6086f1
Dr <yansinan@163.com>
2020-07-16 22:07:47 +0800

    减少包需求

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // deps
 const serve = require('serve')
-const opn = require('opn');
-const chalk = require('chalk');
+// const opn = require('opn');
+// const chalk = require('chalk');
 // vars
 const PORT = 3006;
 const url = `http://127.0.0.1:${PORT}/menu.html`;
@@ -11,5 +11,5 @@ const server = serve(__dirname, {
   ignore: ['node_modules']
 });
 
-opn(url);
-Log(chalk.green(`server run at 127.0.0.1:${PORT}`));
+// opn(url);
+// Log(chalk.green(`server run at 127.0.0.1:${PORT}`));
